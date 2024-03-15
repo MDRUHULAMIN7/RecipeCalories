7a9f96e9c0d946c3bddb4b51c168646528fc3a36
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-03-15 20:35:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+    <h1 className="text-3xl font-bold underline text-red-700 text-primary">
+      Hello world!
+    </h1>
+      
+      
+    </>
+  )
+}
+
+export default App
